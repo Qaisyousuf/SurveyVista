3b98fd109f2b7743ff11c7a47f05819614fb108c
Qais Yousuf <mr.qais.yousuf@gmail.com>
2024-02-22 16:46:42 +0100

    Banner CURD operation completed

--- Web/Areas/Admin/Controllers/BannerController.cs
+++ Web/Areas/Admin/Controllers/BannerController.cs
@@ -60,11 +60,96 @@ namespace Web.Areas.Admin.Controllers
                 await _banner.Add(banner);
 
                 await _banner.commitAsync();
+                TempData["Success"] = "Banner created successfully";
 
                 return RedirectToAction(nameof(Index));
                 
             }
             return View(viewmodel);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var bannerFromdb = _banner.GetBannerById(id);
+
+            var viewmodel = new BannerViewModel
+            {
+                Id=bannerFromdb.Id,
+                Title=bannerFromdb.Title,
+                Description=bannerFromdb.Description,
+                Content=bannerFromdb.Content,
+                ImageUrl=bannerFromdb.ImageUrl,
+                LinkUrl=bannerFromdb.ImageUrl,
+            };
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(BannerViewModel viewmodel)
+        {
+            if(ModelState.IsValid)
+            {
+                var banner = _banner.GetBannerById(viewmodel.Id);
+
+                banner.Title = viewmodel.Title;
+                banner.Content = viewmodel.Content;
+                banner.Description = viewmodel.Description;
+                banner.LinkUrl = viewmodel.LinkUrl;
+                banner.ImageUrl = viewmodel.ImageUrl;
+
+                _banner.Update(banner);
+
+                await _banner.commitAsync();
+                TempData["Success"] = "Banner updated successfully";
+
+                return RedirectToAction(nameof(Index));
+
+            }
+
+              
+            
+
+            return View(viewmodel);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var bannerFromDb = _banner.GetBannerById(id);
+
+            var viewmodel = new BannerViewModel
+            {
+                Id=bannerFromDb.Id,
+                Title=bannerFromDb.Title,
+                Description=bannerFromDb.Description,
+                Content=bannerFromDb.Content,
+                ImageUrl=bannerFromDb.ImageUrl,
+                LinkUrl=bannerFromDb.ImageUrl,
+
+            };
+
+            return View(viewmodel);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+
+            //var banner = _banner.GetBannerById(id);
+
+            _banner.Delete(id);
+
+            await _banner.commitAsync();
+            TempData["Success"] = "Banner deleted successfully";
+
+            return RedirectToAction(nameof(Index));
+
+           
+
+          
         }
     }
 }
