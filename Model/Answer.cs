57b89d6456377d02b4e65d43c4822f085c5b2f40
Qais Yousuf <mr.qais.yousuf@gmail.com>
2024-03-26 17:51:26 +0100

    the questionnaire CURD operation is complete

--- Model/Answer.cs
+++ Model/Answer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,7 +12,9 @@ namespace Model
     public class Answer
     {
         public int Id { get; set; }
-        
+
+       
+        [DisplayName("Answer")]
         public string? Text { get; set; }
         public int QuestionId { get; set; } // Foreign key for Question
         [ForeignKey("QuestionId")]
--- Model/Question.cs
+++ Model/Question.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -14,6 +16,9 @@ namespace Model
             Answers=new List<Answer>();
         }
         public int Id { get; set; }
+
+        [DisplayName("Question")]
+      
         public string? Text { get; set; }
         public QuestionType Type { get; set; }
 
